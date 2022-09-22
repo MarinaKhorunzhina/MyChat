365ce15ba4688a5c703fe6f01caec879c8a5b1f6
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2022-09-22 21:58:25 +0300

    Compositional Layout Overview

---
+++ MyChat/Controllers/PeopleViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PeopleViewController.swift
+//  MyChat
+//
+//  Created by Marina on 22.09.22.
+//
+
+import UIKit
+
+class PeopleViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+    
+}
+
