76a8f46588a3a3d91a88e958d5f297edaf0b5e58
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2022-10-02 13:12:13 +0300

    App Users Screen

---
+++ MyChat/Model/MUser.swift
@@ -0,0 +1,22 @@
+//
+//  MUser.swift
+//  MyChat
+//
+//  Created by Marina on 2.10.22.
+//
+
+import UIKit
+
+struct MUser: Hashable, Decodable {
+    var username: String
+    var avatarStringURL: String
+    var id: Int
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: MUser, rhs: MUser) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
