f92f692e8338dbfb0266eff495c5b6b71f3de68b
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2022-10-05 20:29:37 +0300

    Profile Screen UITextField

---
+++ MyChat/Extension/UIKit/ UIView + Extension.swift
@@ -0,0 +1,23 @@
+//
+//   UIView + Extension.swift
+//  MyChat
+//
+//  Created by Marina on 5.10.22.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func applyGradients(cornerRadius: CGFloat) {
+        self.backgroundColor = nil
+        self.layoutIfNeeded()
+        let gradientView = GradientView(from: .topTrailing, to: .bottomLeading, startColor: #colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1), endColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
+        if let gradientLayer = gradientView.layer.sublayers?.first as? CAGradientLayer {
+            gradientLayer.frame = self.bounds
+            gradientLayer.cornerRadius = cornerRadius
+            
+            self.layer.insertSublayer(gradientLayer, at: 0)
+        }
+    }
+}
